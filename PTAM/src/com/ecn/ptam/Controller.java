bf8290a7833037db19447049009217f90dd989cb
Damien Firmenich <fir.damien@gmail.com>
2014-08-20 13:10:22 +0200

    started controller

---
+++ PTAM/src/com/ecn/ptam/Controller.java
@@ -0,0 +1,17 @@
+package com.ecn.ptam;
+
+public class Controller {
+
+	public Boolean store_location() {
+
+		return true;
+	}
+	
+	public void start_capture() {
+		
+	}
+	
+	public void stop_capture() {
+		
+	}
+}
--- PTAM/src/com/ecn/ptam/GLViewer.java
+++ PTAM/src/com/ecn/ptam/GLViewer.java
@@ -6,13 +6,12 @@ import android.view.View;
 import android.widget.Button;
 
 
-
-
 class GLViewer extends GLSurfaceView implements View.OnClickListener  {
 	private VideoSource _vs;
 	private BatchRenderer _renderer;
+	private Controller _controller;
 	
-	private enum State {INIT, STEREO_STARTED, STEREO_ENDED, BOTTOM_LEFT, TOP_LEFT, GOT_OBJECT};
+	private enum State {INIT, STEREO_STARTED, STEREO_ENDED, GOT_OBJECT, RUNNING};
 	private State _state;
 	
 	public GLViewer(Context context) {
@@ -38,7 +37,7 @@ class GLViewer extends GLSurfaceView implements View.OnClickListener  {
 	public void onPause() {
 		_vs.camera_release();
 	}
-
+	
 	@Override
 	public void onClick(View v) {
 		Button btn = (Button)v;
@@ -51,26 +50,24 @@ class GLViewer extends GLSurfaceView implements View.OnClickListener  {
 			break;
 		case STEREO_STARTED:
 			PTAMWrapper.sendEventPTAM("Space");
-			btn.setText("Bottom left");
+			btn.setText("Store");
 			_state = State.STEREO_ENDED;
 			break;
 		case STEREO_ENDED:
-			PTAMWrapper.sendEventPTAM("Enter");
-			btn.setText("Top left");
-			_state = State.BOTTOM_LEFT;
-			break;
-		case BOTTOM_LEFT:
-			PTAMWrapper.sendEventPTAM("Enter");
-			btn.setText("Top right");
-			_state = State.TOP_LEFT;
-			break;
-		case TOP_LEFT:
-			PTAMWrapper.sendEventPTAM("Enter");
-			btn.setText("Top left");
-			_state = State.GOT_OBJECT;
+			Boolean done = _controller.store_location();
+			if (done) {
+				_state = State.BOTTOM_LEFT;
+				btn.setText("Start");
+			}
 			break;
 		case GOT_OBJECT:
+			_controller.start_capture();
+			btn.setText("Stop");
+			_state = State.RUNNING;
 			break;
+		case RUNNING:
+			_controller.stop_capture();
+			PTAMWrapper.sendEventPTAM("s");
 		default:
 			break;
 		}
